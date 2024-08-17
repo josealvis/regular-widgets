5f74a1292db88fef9fd01b81d4581532461f5b79
Jose Alvis <jalvis1491@gmail.com>
2024-08-17 14:47:37 -0400

    add plantTypes

---
+++ app/widget/my-plant/consts.ts
@@ -0,0 +1,21 @@
+export enum plantTypes {
+    BEET ='beet',
+    CABBAGE = 'cabbage',
+    CARROT = 'carrot',
+    CORN = 'corn',
+    CUCUMBER = 'cucumber',
+    EGGPLANT = 'eggplant',
+    ONION = 'onion',
+    PEAS = 'peas',
+    PEPPER = 'pepper',
+    POTATO = 'potato',
+    PUMPKIN = 'pumpkin',
+    RADISH = 'radish',
+    SALAD = 'salad',
+    SPINACH = 'spinach',
+    TOMAT ='tomat',
+    WATERMELON = 'watermelon',
+    WHEAT = 'wheat'
+
+
+}
